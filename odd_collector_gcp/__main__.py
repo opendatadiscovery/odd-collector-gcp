2d3300a8704ae1485ae42dd7e2e408648bc3c8b6
Mateusz Kulas <60038363+m-qlas@users.noreply.github.com>
2023-07-06 10:14:48 +0200

    feat: adapter for Google Cloud Storage (#18)

--- odd_collector_gcp/__main__.py
+++ odd_collector_gcp/__main__.py
@@ -1,28 +1,14 @@
-import asyncio
-import logging
-from os import path
 from pathlib import Path
 
 from odd_collector_sdk.collector import Collector
 from odd_collector_gcp.domain.plugin import PLUGIN_FACTORY
 
-logging.basicConfig(
-    level=logging.INFO, format="[%(asctime)s] %(levelname)s in %(module)s: %(message)s"
+COLLECTOR_PACKAGE = __package__
+CONFIG_PATH = Path().cwd() / "collector_config.yaml"
+
+collector = Collector(
+    config_path=CONFIG_PATH,
+    root_package=COLLECTOR_PACKAGE,
+    plugin_factory=PLUGIN_FACTORY,
 )
-
-try:
-    loop = asyncio.get_event_loop()
-
-    config_path = Path().cwd() / "collector_config.yaml"
-    root_package = "odd_collector_gcp"
-
-    collector = Collector(config_path, root_package, PLUGIN_FACTORY)
-
-    loop.run_until_complete(collector.register_data_sources())
-
-    collector.start_polling()
-
-    asyncio.get_event_loop().run_forever()
-except Exception as e:
-    logging.error(e, exc_info=True)
-    asyncio.get_event_loop().stop()
+collector.run()
--- odd_collector_gcp/domain/plugin.py
+++ odd_collector_gcp/domain/plugin.py
@@ -1,7 +1,11 @@
 from typing import Literal, Optional
 
 from odd_collector_sdk.domain.plugin import Plugin
+from odd_collector_sdk.domain.filter import Filter
 from odd_collector_sdk.types import PluginFactory
+
+from odd_collector_gcp.adapters.gcs.domain.parameters import GCSAdapterParams
+from odd_collector_gcp.domain.dataset_config import DatasetConfig
 
 
 class GcpPlugin(Plugin):
@@ -17,7 +21,15 @@ class BigTablePlugin(GcpPlugin):
     rows_limit: Optional[int] = 10
 
 
+class GCSPlugin(GcpPlugin):
+    type: Literal["gcs"]
+    datasets: list[DatasetConfig]
+    parameters: Optional[GCSAdapterParams] = None
+    filename_filter: Optional[Filter] = Filter()
+
+
 PLUGIN_FACTORY: PluginFactory = {
-    "bigtable": BigTablePlugin,
     "bigquery_storage": BigQueryStoragePlugin,
+    "bigtable": BigTablePlugin,
+    "gcs": GCSPlugin,
 }
