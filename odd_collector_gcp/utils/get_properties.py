6cb4f2bb9ba3e24a8e6f33ab253e56c897d14f4e
Mateusz Kulas <mkulas@provectus.com>
2023-08-07 15:45:22 +0200

    Refactor 2

--- odd_collector_gcp/utils/get_properties.py
+++ odd_collector_gcp/utils/get_properties.py
@@ -9,10 +9,8 @@ def is_property(parameter: Any) -> bool:
 
 
 def get_properties(instance: Any, excluded_properties=None) -> dict[str, Any]:
-    if excluded_properties is None:
-        excluded_properties = []
     properties = {}
     for prop_name, value in getmembers(instance.__class__, is_property):
         properties[prop_name] = value.fget(instance)
 
-    return omit(properties, excluded_properties)
+    return omit(properties, excluded_properties) if excluded_properties else properties
